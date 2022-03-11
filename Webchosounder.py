10209db8a11b1098de37fa1460a0dfc5d2802345
1ntegrxl <jaigagne@perdu.com>
2022-03-11 13:50:49 +0100

    code refactoring

--- Webchosounder.py
+++ Webchosounder.py
@@ -1,40 +1,45 @@
 #! /usr/bin/env python3
 
-from scapy.all import *
-
-import Echosounder as Echomod
+from typing import List
+import Echosounder
 from flask import Flask, jsonify, render_template, request
+from scapy.arch import get_if_addr
+from scapy.config import conf
 
-IPlocale = get_if_addr(conf.iface)
+LOCAL_IP = get_if_addr(conf.iface)
 
 app = Flask(__name__, template_folder='templates')
 app.config["CACHE_TYPE"] = "null"
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/json/arp_scan', methods=['POST'])
 def scan_arp():
-    if(not ifContainCible(request.json)):
-        return {'error' : "malformed request"}
+    if not if_contain_cible(request.json):
+        return {'error': "malformed request"}
     else:
-        a = Echomod.TEMPLATE()
-        b = Echomod.creation_data_scan_arp(request.json['cible'])
-        return jsonify(local_data=a, scan=b)
+        local_ip_mac_and_gateway: dict = Echosounder.template()
+        local_ip_mac: List[dict] = Echosounder.data_creation_arp_scan(request.json['cible'])
+        return jsonify(local_data=local_ip_mac_and_gateway, scan=local_ip_mac)
+
 
 @app.route('/json/fast_scan', methods=['POST'])
 def scan_rapide():
-    if(not ifContainCible(request.json)):
-        return {'error' : "malformed request"}
+    if not if_contain_cible(request.json):
+        return {'error': "malformed request"}
     else:
-        a = Echomod.TEMPLATE()
-        l = Echomod.creation_data_fast_ping(request.json['cible'])
-        return jsonify(local_data=a, scan=l)
+        local_ip_mac_and_gateway: dict = Echosounder.template()
+        ip_mac_os: List[dict] = Echosounder.data_creation_fast_ping(request.json['cible'])
+        return jsonify(local_data=local_ip_mac_and_gateway, scan=ip_mac_os)
 
-def ifContainCible(testcible):
-    return 'cible' in testcible
+
+def if_contain_cible(test_target) -> bool:
+    return 'cible' in test_target
 
 
 if __name__ == "__main__":
-    app.run(host=IPlocale ,port=5042,debug=True)
+    app.run(host=LOCAL_IP, port=5042, debug=True)
