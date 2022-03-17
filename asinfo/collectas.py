340901351009d7b0123278205605ff8aefe73a16
Wings Music <zzxiang21cn@hotmail.com>
2022-03-17 23:16:43 +0100

    ajout conversion IP to AS fonctionnelle mais pas optimisé

--- asinfo/collectas.py
+++ asinfo/collectas.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import requests, gzip
+import requests, gzip, json
 from datetime import date
 
 def get_prefix2as_data():
@@ -13,14 +13,21 @@ def get_prefix2as_data():
         if(i.startswith('      <a href="')):
             data_url = i[15:53]
             break
-    print(data_url)
     data_response = requests.get(url + data_url).content
-    print(data_response)
     data_response = gzip.decompress(data_response)
     with open('routeviews-prefix2as-latest.pfx2as', 'wb') as filepfx:
         filepfx.write(data_response)
         filepfx.close()
 
+def get_prefix2as_to_json():
+    with open('routeviews-prefix2as-latest.pfx2as', 'r') as filepfx:
+        list_cidr_as = filepfx.readlines()
+        list_cidr_as = [i.rstrip().replace('\t', '/', 1).split('\t') for i in list_cidr_as]
+        filepfx.close()
+        with open('routeviews-prefix2as-latest.json', 'w') as filejson:
+            filejson.write(json.dumps(list_cidr_as))
+            filejson.close()
 
 if __name__ == "__main__":
     get_prefix2as_data()
+    get_prefix2as_to_json()
