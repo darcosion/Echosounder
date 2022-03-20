702701ef30b0ddf812eeba9b79c285df329607c5
haha <daniel@danielmiessler.com>
2022-03-20 20:28:33 +0100

    Added support for rdp scans (route and backend function

--- echosounder.py
+++ echosounder.py
@@ -385,6 +385,21 @@ def scan_snmp_processes(target_ip):
                 })
     return global_list
 
+def scan_rdp_info(target_ip):
+    nm = nmap.PortScanner()  # instantiate nmap.PortScanner object
+    nmap_scan_result: dict = {}
+    nmap_scan_result = nm.scan(target_ip, arguments="-p 3389 --script rdp_ntlm_info")
+    global_list: List[dict] = []
+    for i in nm.all_hosts():
+        for protocol in nm[i].all_protocols():
+            for kport, content in nm[i][protocol].items():
+                global_list.append({
+                    "IP": i,
+                    "protocol" : protocol,
+                    "port" : str(kport),
+                    "result" : nm[i][protocol][kport],
+                })
+    return global_list
 
 def data_creation_services_discovery(target_ip, port_start: int = 0, port_end: int = 400) -> List[dict]:
     """
